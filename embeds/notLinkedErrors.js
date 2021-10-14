279edce562ebecb46b1ec78a6e3d565202f7d194
AVMG20 <rijstenpap1234@hotmail.com>
2021-10-14 19:49:33 +0200

    updated error message

--- embeds/notLinkedErrors.js
+++ embeds/notLinkedErrors.js
@@ -4,13 +4,8 @@ const {colors , controlpanel_url} = require('../config')
 module.exports = {
     ExecutingUserNotLinkedError : new MessageEmbed()
         .setColor(colors.danger)
-        .setTitle(`You can only execute this command if you have linked your Dashboard and Discord accounts`)
+        .setTitle(`Command can only be executed with linked users`)
         .setDescription(`You can link your Dashboard and Discord accounts by going to [dashboard profile](${controlpanel_url}/profile)\n and clicking the 'Link Discord' button`),
-
-    SearchedUserNotLinked : new MessageEmbed()
-        .setColor(colors.danger)
-        .setTitle(`User not linked`)
-        .setDescription(`This user has not linked there Dashboard and Discord account.`),
 }
 
 
